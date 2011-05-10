431a04a7b1f2f8c478eac44ed9aa365f7a1d1d81
Robert Austin <robert@oatlab.com>
2011-05-10 01:00:00 -0400

    Changed uuid to be organized in an OO way

--- test/benchmark.js
+++ test/benchmark.js
@@ -1,5 +1,5 @@
-var nodeuuid = require('../uuid'),
-    uuidjs = require('uuid').generate,
+var nodeuuid = new (require('../uuid'))(),
+    uuidjs = new (require('uuid').generate)(),
     N = 5e5;
 
 function rate(msg, t) {
@@ -9,19 +9,19 @@ function rate(msg, t) {
 }
 
 // node-uuid - string form
-for (var i = 0, t = Date.now(); i < N; i++) nodeuuid();
+for (var i = 0, t = Date.now(); i < N; i++) nodeuuid.generate();
 rate('nodeuuid()', t);
 
-for (var i = 0, t = Date.now(); i < N; i++) nodeuuid('binary');
+for (var i = 0, t = Date.now(); i < N; i++) nodeuuid.generate('binary');
 rate('nodeuuid(\'binary\')', t);
 
 var buffer = new nodeuuid.BufferClass(16);
-for (var i = 0, t = Date.now(); i < N; i++) nodeuuid('binary', buffer);
+for (var i = 0, t = Date.now(); i < N; i++) nodeuuid.generate('binary', buffer);
 rate('nodeuuid(\'binary\', buffer)', t);
 
 // node-uuid - string form
-for (var i = 0, t = Date.now(); i < N; i++) uuidjs();
+for (var i = 0, t = Date.now(); i < N; i++) uuidjs.generate();
 rate('uuidjs()', t);
 
-for (var i = 0, t = Date.now(); i < N; i++) uuidjs('binary');
+for (var i = 0, t = Date.now(); i < N; i++) uuidjs.generate('binary');
 rate('uuidjs(\'binary\')', t);
