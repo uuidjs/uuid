e4cb2bdc713b40d185d00b6430f962bbb8fdea8d
Roman Shtylman <shtylman@gmail.com>
2013-02-24 16:09:52 -0500

    add browser field to package.json
    Pull environment detection of out uuid.js and use the browser field.
    
    This is better for smart bundlers like browserify which will pick up on
    the browser field and package the module intelligently for client side.

--- uuid.js
+++ uuid.js
@@ -6,44 +6,7 @@
 // Unique ID creation requires a high quality random # generator.  We feature
 // detect to determine the best RNG source, normalizing to a function that
 // returns 128-bits of randomness, since that's what's usually required
-var _rng;
-
-// Node.js crypto-based RNG - http://nodejs.org/docs/v0.6.2/api/crypto.html
-//
-// Moderately fast, high quality
-if (typeof(require) == 'function') {
-  try {
-    var _rb = require('crypto').randomBytes;
-    _rng = _rb && function() {return _rb(16);};
-  } catch(e) {}
-}
-
-if (!_rng && global.crypto && crypto.getRandomValues) {
-  // WHATWG crypto-based RNG - http://wiki.whatwg.org/wiki/Crypto
-  //
-  // Moderately fast, high quality
-  var _rnds8 = new Uint8Array(16);
-  _rng = function whatwgRNG() {
-    crypto.getRandomValues(_rnds8);
-    return _rnds8;
-  };
-}
-
-if (!_rng) {
-  // Math.random()-based (RNG)
-  //
-  // If all else fails, use Math.random().  It's fast, but is of unspecified
-  // quality.
-  var  _rnds = new Array(16);
-  _rng = function() {
-    for (var i = 0, r; i < 16; i++) {
-      if ((i & 0x03) === 0) r = Math.random() * 0x100000000;
-      _rnds[i] = r >>> ((i & 0x03) << 3) & 0xff;
-    }
-
-    return _rnds;
-  };
-}
+var _rng = require('./rng');
 
 // Buffer class to use
 var BufferClass = typeof(Buffer) == 'function' ? Buffer : Array;
