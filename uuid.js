e3cc1145aaca96b594ee35dc5001ea50db5472d6
Maurice de Beijer <maurice.de.beijer@gmail.com>
2015-11-13 15:25:41 +0100

    Don't try to load the node package in a browser

--- uuid.js
+++ uuid.js
@@ -18,7 +18,7 @@
   // Node.js crypto-based RNG - http://nodejs.org/docs/v0.6.2/api/crypto.html
   //
   // Moderately fast, high quality
-  if ('function' === typeof require) {
+  if ('undefined' === typeof window && 'function' === typeof require) {
     try {
       var _rb = require('crypto').randomBytes;
       _nodeRNG = _rng = _rb && function() {return _rb(16);};
