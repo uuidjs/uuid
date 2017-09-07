f7ca391539771d659ffde2bd54cd0d977a4f10f2
Robert Kieffer <robert@broofa.com>
2017-09-07 13:58:38 -0700

    eslint

--- test/test.js
+++ test/test.js
@@ -14,7 +14,7 @@ var HASH_SAMPLES = [
 
   // Extended ascii chars
   {
-    input: '\t\b\f  !\"#$%&\'()*+,-.\/0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\u00A1\u00A2\u00A3\u00A4\u00A5\u00A6\u00A7\u00A8\u00A9\u00AA\u00AB\u00AC\u00AE\u00AF\u00B0\u00B1\u00B2\u00B3\u00B4\u00B5\u00B6\u00B7\u00B8\u00B9\u00BA\u00BB\u00BC\u00BD\u00BE\u00BF\u00C0\u00C1\u00C2\u00C3\u00C4\u00C5\u00C6\u00C7\u00C8\u00C9\u00CA\u00CB\u00CC\u00CD\u00CE\u00CF\u00D0\u00D1\u00D2\u00D3\u00D4\u00D5\u00D6\u00D7\u00D8\u00D9\u00DA\u00DB\u00DC\u00DD\u00DE\u00DF\u00E0\u00E1\u00E2\u00E3\u00E4\u00E5\u00E6\u00E7\u00E8\u00E9\u00EA\u00EB\u00EC\u00ED\u00EE\u00EF\u00F0\u00F1\u00F2\u00F3\u00F4\u00F5\u00F6\u00F7\u00F8\u00F9\u00FA\u00FB\u00FC\u00FD\u00FE\u00FF',
+    input: '\t\b\f  !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\u00A1\u00A2\u00A3\u00A4\u00A5\u00A6\u00A7\u00A8\u00A9\u00AA\u00AB\u00AC\u00AE\u00AF\u00B0\u00B1\u00B2\u00B3\u00B4\u00B5\u00B6\u00B7\u00B8\u00B9\u00BA\u00BB\u00BC\u00BD\u00BE\u00BF\u00C0\u00C1\u00C2\u00C3\u00C4\u00C5\u00C6\u00C7\u00C8\u00C9\u00CA\u00CB\u00CC\u00CD\u00CE\u00CF\u00D0\u00D1\u00D2\u00D3\u00D4\u00D5\u00D6\u00D7\u00D8\u00D9\u00DA\u00DB\u00DC\u00DD\u00DE\u00DF\u00E0\u00E1\u00E2\u00E3\u00E4\u00E5\u00E6\u00E7\u00E8\u00E9\u00EA\u00EB\u00EC\u00ED\u00EE\u00EF\u00F0\u00F1\u00F2\u00F3\u00F4\u00F5\u00F6\u00F7\u00F8\u00F9\u00FA\u00FB\u00FC\u00FD\u00FE\u00FF',
     sha1: 'ca4a426a3d536f14cfd79011e79e10d64de950a0',
     md5: 'e8098ec21950f841731d28749129d3ee',
   },
@@ -92,15 +92,17 @@ test('v3', function() {
   var buf = new Array(16);
   var testBuf = [0x91, 0x25, 0xa8, 0xdc, 0x52, 0xee, 0x36, 0x5b, 0xa5, 0xaa, 0x81, 0xb0, 0xb3, 0x68, 0x1c, 0xf6];
   v3('hello.example.com', v3.DNS, buf);
-  assert.ok(buf.length === testBuf.length && buf.every(function (elem, idx) { return elem === testBuf[idx]; }));
+  assert.ok(buf.length === testBuf.length && buf.every(function(elem, idx) {
+    return elem === testBuf[idx];
+  }));
 
   // test offsets as well
   buf = new Array(19);
   for (var i=0; i<3; ++i) buf[i] = 'landmaster';
   v3('hello.example.com', v3.DNS, buf, 3);
-  assert.ok(buf.length === testBuf.length+3 && buf.every(function (elem, idx) {
+  assert.ok(buf.length === testBuf.length+3 && buf.every(function(elem, idx) {
     return (idx >= 3) ? (elem === testBuf[idx-3]) : (elem === 'landmaster');
-  }), "hello");
+  }), 'hello');
 });
 
 test('v5', function() {
@@ -115,13 +117,15 @@ test('v5', function() {
   var buf = new Array(16);
   var testBuf = [0xfd, 0xda, 0x76, 0x5f, 0xfc, 0x57, 0x56, 0x04, 0xa2, 0x69, 0x52, 0xa7, 0xdf, 0x81, 0x64, 0xec];
   v5('hello.example.com', v5.DNS, buf);
-  assert.ok(buf.length === testBuf.length && buf.every(function (elem, idx) { return elem === testBuf[idx]; }));
+  assert.ok(buf.length === testBuf.length && buf.every(function(elem, idx) {
+    return elem === testBuf[idx];
+  }));
 
   // test offsets as well
   buf = new Array(19);
   for (var i=0; i<3; ++i) buf[i] = 'landmaster';
   v5('hello.example.com', v5.DNS, buf, 3);
-  assert.ok(buf.length === testBuf.length+3 && buf.every(function (elem, idx) {
+  assert.ok(buf.length === testBuf.length+3 && buf.every(function(elem, idx) {
     return (idx >= 3) ? (elem === testBuf[idx-3]) : (elem === 'landmaster');
   }));
 });
