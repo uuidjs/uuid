010a6af0b23f60969f430a15a33795fc42b52196
Christoph Tavan <dev@tavan.de>
2019-09-23 23:54:56 +0200

    feat: add various es module and CommonJS examples

---
+++ examples/browser-webpack/webpack.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  entry: {
+    all: './example-all.js',
+    v1: './example-v1.js',
+    v4: './example-v4.js',
+  },
+  output: {
+    filename: '[name].js',
+  },
+  mode: 'production'
+};
