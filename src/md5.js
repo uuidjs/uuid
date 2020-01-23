b59b5c5ecad271c5453f1a156f011671f6d35627
Christoph Tavan <dev@tavan.de>
2020-01-23 13:12:51 +0100

    feat: remove support for pre Node.js v4 Buffer API (#356)
    BREAKING CHANGE: Remove support for generating v3 and v5 UUIDs in
    Node.js<4.x

--- src/md5.js
+++ src/md5.js
@@ -1,20 +1,10 @@
 import crypto from 'crypto';
 
 function md5(bytes) {
-  if (typeof Buffer.from === 'function') {
-    // Modern Buffer API
-    if (Array.isArray(bytes)) {
-      bytes = Buffer.from(bytes);
-    } else if (typeof bytes === 'string') {
-      bytes = Buffer.from(bytes, 'utf8');
-    }
-  } else {
-    // Pre-v4 Buffer API
-    if (Array.isArray(bytes)) {
-      bytes = new Buffer(bytes);
-    } else if (typeof bytes === 'string') {
-      bytes = new Buffer(bytes, 'utf8');
-    }
+  if (Array.isArray(bytes)) {
+    bytes = Buffer.from(bytes);
+  } else if (typeof bytes === 'string') {
+    bytes = Buffer.from(bytes, 'utf8');
   }
 
   return crypto
