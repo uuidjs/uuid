20ec029408ca9b441a11cc32bf2d287fa6f5df9a
Christoph Tavan <dev@tavan.de>
2020-02-27 13:34:53 +0100

    fix: simplify link in deprecation warning

--- v5.js
+++ v5.js
@@ -4,5 +4,5 @@ const v5 = require('./dist/v5.js');
 
 module.exports = util.deprecate(
   v5,
-  "Deep requiring like `const uuidv5 = require('uuid/v5');` is deprecated as of uuid@7.x. Please require the top-level module when using the Node.js CommonJS module or use ECMAScript Modules when bundling for the browser. See https://github.com/uuidjs/uuid/blob/master/README.md#upgrading-from-uuid3 for more information.",
+  "Deep requiring like `const uuidv5 = require('uuid/v5');` is deprecated as of uuid@7.x. Please require the top-level module when using the Node.js CommonJS module or use ECMAScript Modules when bundling for the browser. See https://github.com/uuidjs/uuid#deep-requires-now-deprecated for more information.",
 );
