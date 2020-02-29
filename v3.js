bb2c8e4e9f4c5f9c1eaaf3ea59710c633cd90cb7
Christoph Tavan <dev@tavan.de>
2020-02-29 20:32:46 +0100

    fix: simplify link in deprecation warning (#391)

--- v3.js
+++ v3.js
@@ -4,5 +4,5 @@ const v3 = require('./dist/v3.js');
 
 module.exports = util.deprecate(
   v3,
-  "Deep requiring like `const uuidv3 = require('uuid/v3');` is deprecated as of uuid@7.x. Please require the top-level module when using the Node.js CommonJS module or use ECMAScript Modules when bundling for the browser. See https://github.com/uuidjs/uuid/blob/master/README.md#upgrading-from-uuid3 for more information.",
+  "Deep requiring like `const uuidv3 = require('uuid/v3');` is deprecated as of uuid@7.x. Please require the top-level module when using the Node.js CommonJS module or use ECMAScript Modules when bundling for the browser. See https://github.com/uuidjs/uuid#deep-requires-now-deprecated for more information.",
 );
