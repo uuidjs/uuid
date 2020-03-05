ba65bd1fe0b940805169ed6ab08ff60adc117f0b
Christoph Tavan <dev@tavan.de>
2020-03-05 16:11:33 +0100

    test: use webdriverio (#399)
    Our current browser tests were incredibly flaky.
    
    Webdriver.io seems to contain retry logic and other safeguards that we
    are currently lacking in our custom browserstack-local /
    selenium-webdriver setup.
    
    Under the hood things should still work similarly, let's see if they are
    now more stable.
    
    Using the new setup I was able to observer 10 successful BrowserStack CI runs
    in a row – which has never happened with the previous setup.

---
+++ wdio.conf.js
@@ -0,0 +1,133 @@
+/* eslint-disable camelcase */
+
+const PORT = 9000;
+const PROJECT = process.env.GITHUB_REPOSITORY || 'node-uuid';
+const GITHUB_SHA = process.env.GITHUB_SHA || '';
+const GITHUB_REF = process.env.GITHUB_REF || '';
+const BUILD = GITHUB_SHA || GITHUB_REF ? `${GITHUB_REF} ${GITHUB_SHA}` : 'manual build';
+
+const commonCapabilities = {
+  project: PROJECT,
+  build: BUILD,
+  name: 'browser test',
+  'browserstack.local': true,
+  'browserstack.debug': false,
+  resolution: '1024x768',
+};
+
+const capabilities = [
+  // Chrome
+  {
+    ...commonCapabilities,
+    browserName: 'Chrome',
+    browser_version: '80.0',
+    os: 'Windows',
+    os_version: '10',
+  },
+  {
+    ...commonCapabilities,
+    browserName: 'Chrome',
+    browser_version: '49.0',
+    os: 'Windows',
+    os_version: '10',
+  },
+
+  // Firefox
+  {
+    ...commonCapabilities,
+    browserName: 'Firefox',
+    browser_version: '73.0',
+    os: 'Windows',
+    os_version: '10',
+  },
+  {
+    ...commonCapabilities,
+    browserName: 'Firefox',
+    browser_version: '44.0',
+    os: 'Windows',
+    os_version: '10',
+  },
+
+  // Safari
+  {
+    ...commonCapabilities,
+    browserName: 'Safari',
+    browser_version: '13.0',
+    os: 'OS X',
+    os_version: 'Catalina',
+  },
+  {
+    ...commonCapabilities,
+    browserName: 'Safari',
+    browser_version: '10.0',
+    os: 'OS X',
+    os_version: 'Sierra',
+  },
+
+  // Edge
+  {
+    ...commonCapabilities,
+    browserName: 'Edge',
+    browser_version: '80.0',
+    os: 'Windows',
+    os_version: '10',
+  },
+  {
+    ...commonCapabilities,
+    browserName: 'Edge',
+    browser_version: '18.0',
+    os: 'Windows',
+    os_version: '10',
+  },
+  {
+    ...commonCapabilities,
+    browserName: 'Edge',
+    browser_version: '15.0',
+    os: 'Windows',
+    os_version: '10',
+  },
+
+  // IE
+  {
+    ...commonCapabilities,
+    browserName: 'IE',
+    browser_version: '11.0',
+    os: 'Windows',
+    os_version: '10',
+  },
+  {
+    ...commonCapabilities,
+    browserName: 'IE',
+    browser_version: '11.0',
+    os: 'Windows',
+    os_version: '7',
+  },
+];
+
+exports.config = {
+  specs: ['./test/browser/browser.spec.js'],
+
+  user: process.env.BROWSERSTACK_USER,
+  key: process.env.BROWSERSTACK_ACCESS_KEY,
+
+  services: ['browserstack', 'static-server'],
+  browserstackLocal: true,
+  staticServerFolders: [{ mount: '/', path: './examples' }],
+  staticServerPort: PORT,
+
+  runner: 'local',
+  maxInstances: 5,
+
+  capabilities,
+
+  logLevel: 'warn',
+  bail: 1,
+  waitforTimeout: 10000,
+  connectionRetryTimeout: 90000,
+  connectionRetryCount: 3,
+  framework: 'jasmine',
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 30000,
+  },
+  reporters: ['spec'],
+};
