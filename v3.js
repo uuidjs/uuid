48b30cc34da11ce4cb332854e1f923cf713ac515
Christoph Tavan <dev@tavan.de>
2020-03-30 17:35:03 +0200

    fix: make deep require deprecation warning work in browsers
    Relates to #408

--- v3.js
+++ v3.js
@@ -1,8 +1,8 @@
-const util = require('util');
+const deprecate = require('./deprecate.js');
 
 const v3 = require('./dist/v3.js');
 
-module.exports = util.deprecate(
+module.exports = deprecate(
   v3,
   "Deep requiring like `const uuidv3 = require('uuid/v3');` is deprecated as of uuid@7.x. Please require the top-level module when using the Node.js CommonJS module or use ECMAScript Modules when bundling for the browser. See https://github.com/uuidjs/uuid#deep-requires-now-deprecated for more information.",
 );
--- v4.js
+++ v4.js
@@ -1,8 +1,8 @@
-const util = require('util');
+const deprecate = require('./deprecate.js');
 
 const v4 = require('./dist/v4.js');
 
-module.exports = util.deprecate(
+module.exports = deprecate(
   v4,
   "Deep requiring like `const uuidv4 = require('uuid/v4');` is deprecated as of uuid@7.x. Please require the top-level module when using the Node.js CommonJS module or use ECMAScript Modules when bundling for the browser. See https://github.com/uuidjs/uuid#deep-requires-now-deprecated for more information.",
 );
