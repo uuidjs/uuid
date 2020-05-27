fc6cc23e0abe677b67570ca3533fb307766ca87e
Ignat Prokopovich <ignatius.awwit@gmail.com>
2020-05-28 02:42:31 +0300

    Update src/regex.js
    Co-authored-by: Robert Kieffer <robert@broofa.com>

--- src/regex.js
+++ src/regex.js
@@ -1,3 +1,3 @@
 const uuidRegex = /^([0-9a-f]{8}-[0-9a-f]{4}-[1345][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12})|((00000000-0000-0000-0000-000000000000))$/i;
 
-export default uuidRegex;
+export default REGEX;
