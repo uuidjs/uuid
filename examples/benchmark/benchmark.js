a04e6cd0af79b2146f989814bd8cf76fae180dbf
Robert Kieffer <robert@broofa.com>
2020-06-23 11:27:40 -0700

    chore: stringify unit test and benchmark

--- examples/benchmark/benchmark.js
+++ examples/benchmark/benchmark.js
@@ -6,17 +6,41 @@ const uuidv4 = (typeof window !== 'undefined' && window.uuidv4) || require('uuid
 const uuidv3 = (typeof window !== 'undefined' && window.uuidv3) || require('uuid').v3;
 const uuidv5 = (typeof window !== 'undefined' && window.uuidv5) || require('uuid').v5;
 const uuidParse = (typeof window !== 'undefined' && window.uuidParse) || require('uuid').parse;
+const uuidStringify =
+  (typeof window !== 'undefined' && window.uuidStringify) || require('uuid').stringify;
 
 console.log('Starting. Tests take ~1 minute to run ...');
 
-function testUuidToBytes() {
+function testParse() {
   const suite = new Benchmark.Suite({
     onError(event) {
       console.error(event.target.error);
     },
   });
 
+  const BYTES = [
+    0x0f,
+    0x5a,
+    0xbc,
+    0xd1,
+    0xc1,
+    0x94,
+    0x47,
+    0xf3,
+    0x90,
+    0x5b,
+    0x2d,
+    0xf7,
+    0x26,
+    0x3a,
+    0x08,
+    0x4b,
+  ];
+
   suite
+    .add('uuidStringify()', function () {
+      uuidStringify(BYTES);
+    })
     .add('uuidParse()', function () {
       uuidParse('0f5abcd1-c194-47f3-905b-2df7263a084b');
     })
@@ -72,5 +96,5 @@ function testGeneration() {
     .run();
 }
 
-testUuidToBytes();
+testParse();
 testGeneration();
