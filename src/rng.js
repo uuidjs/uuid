17ccf234df99d265290c565f264d364775c0d293
Andrey Pechkurov <apechkurov@gmail.com>
2020-08-27 20:48:20 +0300

    perf(nodejs): introduce pool into default rng

--- src/rng.js
+++ src/rng.js
@@ -1,7 +1,14 @@
 import crypto from 'crypto';
 
-const rnds8 = new Uint8Array(16);
+const poolSize = 4;
+const rnds8Pool = new Uint8Array(16 * poolSize);
+let poolPtr = -1;
 
 export default function rng() {
-  return crypto.randomFillSync(rnds8);
+  if (poolPtr === -1 || poolPtr === poolSize) {
+    crypto.randomFillSync(rnds8Pool);
+    poolPtr = 0;
+  }
+  const pos = 16 * poolPtr++;
+  return rnds8Pool.slice(pos, pos + 16);
 }
