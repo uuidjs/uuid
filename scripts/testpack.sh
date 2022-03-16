e99c2f6a96b00e14186299173b08296ad9c9b8d5
Christoph Tavan <dev@tavan.de>
2022-03-16 23:42:06 +0100

    fixup! fix: run node-esmodule test only in latest node LTS

--- scripts/testpack.sh
+++ scripts/testpack.sh
@@ -10,6 +10,7 @@ mkdir -p ../test-pack
 
 cp examples/node-commonjs/example.js ../test-pack/commonjs.js
 cp examples/node-esmodules/example.mjs ../test-pack/esmodules.mjs
+cp examples/node-esmodules/package.mjs ../test-pack/esmodules-package.mjs
 
 cd ../test-pack
 
@@ -18,8 +19,9 @@ npm init -y
 npm install ../uuid/uuid-*.tgz
 
 node commonjs.js
+node esmodules.js
 
 # Support for json esm imports requires import assertions starting in Node.js v16.14.0 which were
 # not supported in earlier versions. Therefore we restrict the ESM test to more recent versions of
 # Node.js:
-( node --version | grep -vq 'v16' ) || ( node --experimental-json-modules esmodules.mjs )
+( node --version | grep -vq 'v16' ) || ( node --experimental-json-modules esmodules-package.mjs )
