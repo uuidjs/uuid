04b4151126526bf3a67253e37490ede2109f11fb
Brian Hung <brianlhung@gmail.com>
2022-10-12 14:36:18 -0700

    export uuid regex

--- src/validate.js
+++ src/validate.js
@@ -1,7 +1,10 @@
-import REGEX from './regex.js';
+import regex from './regex.js';
+
+// Create RegExp that strictly matches uuid format from start to end of line.
+const uuidRegEx = new RegExp(`^(?:${regex.source})$`, 'i');
 
 function validate(uuid) {
-  return typeof uuid === 'string' && REGEX.test(uuid);
+  return typeof uuid === 'string' && uuidRegEx.test(uuid);
 }
 
 export default validate;
