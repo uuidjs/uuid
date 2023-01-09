f87d878eaf78baa73caaa35a21251f08321c0e0c
Michael Di Prisco <cadienvan@gmail.com>
2023-01-09 20:50:17 +0100

    perf: unsafe stringify now uses concatenation

--- src/stringify.js
+++ src/stringify.js
@@ -13,28 +13,28 @@ for (let i = 0; i < 256; ++i) {
 export function unsafeStringify(arr, offset = 0) {
   // Note: Be careful editing this code!  It's been tuned for performance
   // and works in ways you may not expect. See https://github.com/uuidjs/uuid/pull/434
-  return (
-    byteToHex[arr[offset + 0]] +
-    byteToHex[arr[offset + 1]] +
-    byteToHex[arr[offset + 2]] +
-    byteToHex[arr[offset + 3]] +
-    '-' +
-    byteToHex[arr[offset + 4]] +
-    byteToHex[arr[offset + 5]] +
-    '-' +
-    byteToHex[arr[offset + 6]] +
-    byteToHex[arr[offset + 7]] +
-    '-' +
-    byteToHex[arr[offset + 8]] +
-    byteToHex[arr[offset + 9]] +
-    '-' +
-    byteToHex[arr[offset + 10]] +
-    byteToHex[arr[offset + 11]] +
-    byteToHex[arr[offset + 12]] +
-    byteToHex[arr[offset + 13]] +
-    byteToHex[arr[offset + 14]] +
-    byteToHex[arr[offset + 15]]
-  ).toLowerCase();
+  let uuid = '';
+  uuid += byteToHex[arr[offset + 0]];
+  uuid += byteToHex[arr[offset + 1]];
+  uuid += byteToHex[arr[offset + 2]];
+  uuid += byteToHex[arr[offset + 3]];
+  uuid += '-';
+  uuid += byteToHex[arr[offset + 4]];
+  uuid += byteToHex[arr[offset + 5]];
+  uuid += '-';
+  uuid += byteToHex[arr[offset + 6]];
+  uuid += byteToHex[arr[offset + 7]];
+  uuid += '-';
+  uuid += byteToHex[arr[offset + 8]];
+  uuid += byteToHex[arr[offset + 9]];
+  uuid += '-';
+  uuid += byteToHex[arr[offset + 10]];
+  uuid += byteToHex[arr[offset + 11]];
+  uuid += byteToHex[arr[offset + 12]];
+  uuid += byteToHex[arr[offset + 13]];
+  uuid += byteToHex[arr[offset + 14]];
+  uuid += byteToHex[arr[offset + 15]];
+  return uuid.toLowerCase();
 }
 
 function stringify(arr, offset = 0) {
