63ac9fef7be3c828be209a89259301bee224cfe1
Linus Unnebäck <linus@folkdatorn.se>
2023-04-12 12:36:46 +0800

    test: remove missing getRandomValues test

--- test/unit/rng.test.js
+++ test/unit/rng.test.js
@@ -1,6 +1,5 @@
 import assert from 'assert';
 import rng from '../../src/rng.js';
-import rngBrowser from '../../src/rng-browser.js';
 
 describe('rng', () => {
   test('Node.js RNG', () => {
@@ -10,12 +9,6 @@ describe('rng', () => {
     for (let i = 0; i < bytes.length; ++i) {
       assert.equal(typeof bytes[i], 'number');
     }
-  });
-
-  test('Browser without crypto.getRandomValues()', () => {
-    assert.throws(() => {
-      rngBrowser();
-    });
   });
 
   // Test of whatwgRNG missing for now since with esmodules we can no longer manipulate the
