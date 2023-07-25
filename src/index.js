876de7b4819793e5477fc5ffbefab8c78e51b19f
Alfonso Villalobos <alfonsocvu@gmail.com>
2023-07-25 15:27:38 -0500

    Fix problems with jest

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-export { default as v1 } from './v1.js';
-export { default as v3 } from './v3.js';
-export { default as v4 } from './v4.js';
-export { default as v5 } from './v5.js';
-export { default as NIL } from './nil.js';
-export { default as version } from './version.js';
-export { default as validate } from './validate.js';
-export { default as stringify } from './stringify.js';
-export { default as parse } from './parse.js';
+import { default as v1 } from './v1.js';
+import { default as v3 } from './v3.js';
+import { default as v4 } from './v4.js';
+import { default as v5 } from './v5.js';
+import { default as NIL } from './nil.js';
+import { default as version } from './version.js';
+import { default as validate } from './validate.js';
+import { default as stringify } from './stringify.js';
+import { default as parse } from './parse.js';
+
+
+export {
+    v1, v3, v4, v5, NIL, version, validate, stringify, parse
+}
