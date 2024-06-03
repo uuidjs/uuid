88e747d3623ee658c3309e8aedc4bfe41b45273e
Robert Kieffer <robert@broofa.com>
2024-06-03 00:49:47 -0700

    chore: update to eslint flat config

---
+++ eslint.config.cjs
@@ -0,0 +1,38 @@
+const babelParser = require('@babel/eslint-parser');
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+const globals = require('globals');
+const js = require('@eslint/js');
+const neostandard = require('neostandard')({ semi: true, noStyle: true });
+
+module.exports = [
+  js.configs.recommended,
+  ...neostandard,
+  eslintPluginPrettierRecommended,
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.commonjs,
+        ...globals.jest,
+        ...globals.node,
+        msCrypto: true,
+      },
+      parser: babelParser,
+    },
+    rules: {
+      'no-var': ['error'],
+    },
+  },
+  {
+    ignores: [
+      '!.babelrc.js',
+      '.local/',
+      '**/dist/',
+      'node_modules/',
+      // We have to ignore this until @babel/eslint-parser supports import assertions
+      '**/package.mjs',
+    ],
+  },
+];
