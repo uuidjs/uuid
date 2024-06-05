5df758ee2ad129570807cff76ab2abbc9d0338f7
Robert Kieffer <robert@broofa.com>
2024-06-05 06:08:37 -0700

    chore: update comments

--- src/v6.js
+++ src/v6.js
@@ -10,23 +10,21 @@ import v1ToV6 from './v1ToV6.js';
  * @returns
  */
 export default function v6(options = {}, buf, offset = 0) {
-  // v6 is v1 with different field layout, so we start with a v1 UUID.
+  // v6 is v1 with different field layout, so we start with a v1 UUID, albeit
+  // with slightly different behavior around how the clock_seq and node fields
+  // are randomized, which is why we call v1 with _v6: true.
   let bytes = v1({ ...options, _v6: true }, new Uint8Array(16));
 
-  // Reorder the fields to v6 layout.  Per RFC9562, we randomize the clock_seq
-  // and node fields for each UUID.  Randomize node and clock_seq here to get
-  // the RFC-recommended behavior (we restore these below if they're provided in
-  // options.)
+  // Reorder the fields to v6 layout.
   bytes = v1ToV6(bytes);
 
+  // Return as a byte array if requested
   if (buf) {
-    // Copy to buffer
     for (let i = 0; i < 16; i++) {
       buf[offset + i] = bytes[i];
     }
     return buf;
   }
 
-  // Otherwise return UUID as a string
   return unsafeStringify(bytes);
 }
