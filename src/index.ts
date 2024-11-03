10225d93d2ae010753966667801b84781bb416bd
Cooper Benson <cooper.benson@quizlet.com>
2024-11-03 12:14:23 -0700

    fix: exporting types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+export type {
+  UUIDTypes,
+  Version1Options,
+  Version4Options,
+  Version6Options,
+  Version7Options,
+} from './_types.js';
 export { default as MAX } from './max.js';
 export { default as NIL } from './nil.js';
 export { default as parse } from './parse.js';
