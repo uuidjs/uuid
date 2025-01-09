e426aaa97527a41e9a033ce70d57995c437a9e06
Robert Kieffer <robert@broofa.com>
2025-01-09 11:31:51 -1000

    build: use `npm pack` tarball for local examples and testing instead of .local (#859)

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,38 +9,40 @@
 # Note: that the "preferred" build for testing (local and CI) is the ESM build,
 # except where we specifically test the other builds
 
+set -e # exit on error
+
 # Change to project root
 ROOT="$(pwd)/$(dirname "$0")/.."
 cd "$ROOT" || exit 1
 
-DIR="$ROOT/dist"
-
-# Clean output dir
-rm -rf "$DIR"
-mkdir -p "$DIR"
+# Prep TS output dir
+DIST_DIR="$ROOT/dist"
+rm -rf "$DIST_DIR"
+mkdir -p "$DIST_DIR"
 
 # Build each module type
 for MODULE_TYPE in esm cjs; do
   echo "Building ${MODULE_TYPE}"
 
-  DIST_DIR="$DIR/${MODULE_TYPE}"
-  BROWSER_DIR="$DIR/${MODULE_TYPE}-browser"
+  NODE_DIST_DIR="$DIST_DIR/${MODULE_TYPE}"
+  BROWSER_DIST_DIR="$DIST_DIR/${MODULE_TYPE}-browser"
 
   tsc -p tsconfig.${MODULE_TYPE}.json
 
   # Clone files for browser builds
-  cp -pr ${DIST_DIR} ${BROWSER_DIR}
+  cp -pr ${NODE_DIST_DIR} ${BROWSER_DIST_DIR}
 
   # Remove browser files from non-browser builds
-  for FILE in ${DIST_DIR}/*-browser*;do
+  for FILE in ${NODE_DIST_DIR}/*-browser*;do
     rm -f $FILE
   done
 
   # Move browser files into place for browser builds
   (
-    # Temporarily cd into BROWSER_DIR to avoid having to deal with "-browser"
-    # appearing in both the dir name and file name of FILE's full path
-    cd ${BROWSER_DIR}
+    # Temporarily cd into BROWSER_DIST_DIR to avoid having to deal with
+    # "-browser" appearing in both the dir name and file name of FILE's full
+    # path
+    cd ${BROWSER_DIST_DIR}
 
     for FILE in *-browser*;do
       mv $FILE ${FILE/-browser/}
@@ -50,12 +52,24 @@ for MODULE_TYPE in esm cjs; do
   # esm/cjs-specific logic
   if [ "$MODULE_TYPE" = "esm" ]; then
     # ESM: copy bin files to dist
-    cp -pr "$DIR/../src/bin" "$DIST_DIR"
+    cp -pr "$DIST_DIR/../src/bin" "$NODE_DIST_DIR"
   else
     # CJS: Add package.json that specifies type: commonjs
-    echo "{\"type\":\"commonjs\"}" > "$DIST_DIR/package.json"
-    echo "{\"type\":\"commonjs\"}" > "$BROWSER_DIR/package.json"
+    echo "{\"type\":\"commonjs\"}" > "$NODE_DIST_DIR/package.json"
+    echo "{\"type\":\"commonjs\"}" > "$BROWSER_DIST_DIR/package.json"
   fi
 done
+
+if [ "${1-}" != "--no-pack" ]; then
+  # Prep tarball dir
+  BUILD_DIR="$ROOT/.build"
+  mkdir -p "$BUILD_DIR"
+  rm -rf "$BUILD_DIR/uuid-*.tgz"
+
+  # Create tarball for local installation (in tests and examples)
+  echo "Packing tarball"
+  npm pack --pack-destination "$BUILD_DIR" > /dev/null 2>&1
+  mv $BUILD_DIR/uuid-*.tgz $BUILD_DIR/uuid.tgz
+fi
 
 echo "-- fin --"
