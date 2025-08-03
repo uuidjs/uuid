64dbe30a0c44bf0e410c47b0879ce05f65635469
Robert Kieffer <robert@broofa.com>
2025-08-03 09:09:13 -0700

    feat: make rng and rng-browser implementations a bit more consistent

--- src/rng-browser.ts
+++ src/rng-browser.ts
@@ -1,13 +1,9 @@
-// Unique ID creation requires a high quality random # generator. In the browser we therefore
-// require the crypto API and do not support built-in fallback to lower quality random number
-// generators (like Math.random()).
-
 let getRandomValues: typeof crypto.getRandomValues | undefined;
 
 const rnds8 = new Uint8Array(16);
 
 export default function rng() {
-  // lazy load so that environments that need to polyfill have a chance to do so
+  // lazy-reference getRandomValues in case it's been polyfilled
   if (!getRandomValues) {
     if (typeof crypto === 'undefined' || !crypto.getRandomValues) {
       throw new Error(
--- src/rng.ts
+++ src/rng.ts
@@ -2,11 +2,16 @@
 // can switch to `globalThis.crypto` and combine `rng.ts` and `rng-browser.ts`.
 import { webcrypto } from 'node:crypto';
 
-const getRandomValues = webcrypto.getRandomValues.bind(webcrypto);
+let getRandomValues: typeof webcrypto.getRandomValues | undefined;
 const rnds8Pool = new Uint8Array(256); // # of random values to pre-allocate
 let poolPtr = rnds8Pool.length;
 
 export default function rng() {
+  // lazy-reference getRandomValues in case it's been polyfilled
+  if (!getRandomValues) {
+    getRandomValues = webcrypto.getRandomValues.bind(webcrypto);
+  }
+
   if (poolPtr > rnds8Pool.length - 16) {
     getRandomValues(rnds8Pool);
     poolPtr = 0;
